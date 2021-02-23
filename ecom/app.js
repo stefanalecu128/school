5e973578c4db36e4cf7151c33512d89c902392b7
stefanalecu128 <stefaanalecu@gmail.com>
2021-02-23 19:56:19 +0200

    another

---
+++ ecom/app.js
@@ -0,0 +1,9 @@
+// Adauga in cos
+
+// let cosStr = localStorage.getItem("cos");
+// let cos = [];
+// if(cosStr!==null){
+// 	cos = JSON.parse(cosStr);
+// }
+// cos.push({produs:2,cantitate:7});
+// localStorage.setItem("cos", JSON.stringify(cos));
