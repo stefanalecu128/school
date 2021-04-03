2f5ec5964a2466d02a4c866b6084cf9e22573d12
stefanalecu128 <stefaanalecu@gmail.com>
2021-04-03 22:41:20 +0300

    Update app.js

--- agenda/app.js
+++ agenda/app.js
@@ -1,134 +1,139 @@
 let person = [
-    {
-      name: "Jean Petrea",
-      nrTel: "0373664897"
+  {
+    name: "Jean Petrea",
+    nrTel: "0373664897"
+  },
+  {
+    name: "Gicu Grigore",
+    nrTel: "0331592849"
+  },
+  {
+    name: "Răducu Simion",
+    nrTel: "0318003189"
+  },
+  {
+    name: "Sergiu Simionescu",
+    nrTel: "0723213153"
+  },
+  {
+      name: "Dana Marian",
+      nrTel: "0743732465"
     },
     {
-      name: "Gicu Grigore",
-      nrTel: "0331592849"
+      name: "Antonia Iordache",
+      nrTel: "0797058887"
     },
     {
-      name: "Răducu Simion",
-      nrTel: "0318003189"
+      name: "Theodor Giurgiu",
+      nrTel: "0780293560"
     },
     {
-      name: "Sergiu Simionescu",
-      nrTel: "0723213153"
-    },
-    {
-        name: "Dana Marian",
-        nrTel: "0743732465"
-      },
-      {
-        name: "Antonia Iordache",
-        nrTel: "0797058887"
-      },
-      {
-        name: "Theodor Giurgiu",
-        nrTel: "0780293560"
-      },
-      {
-        name: "Stan Boboc",
-        nrTel: "0793976689"
-      }
-  ];
-  
-  let index = -1;
-
-  function draw() {
-    let str = "";
-    for (let i=0; i < person.length; i++) {
-      str +=
-        `
-         <tr>
-         <td>${person[i].name}</td>
-         <td>${person[i].nrTel}</td>
-         <td><i class="far fa-edit" onclick="edit(${i});"></i></td>
-         <td><i class="far fa-trash-alt" onclick="del(${i});"></i></td>
-        </tr>
-        `
+      name: "Stan Boboc",
+      nrTel: "0793976689"
     }
-    document.querySelector("#addPerson tbody").innerHTML = str;
+];
+
+let index = -1;
+
+function draw() {
+  let str = "";
+  for (let i=0; i < person.length; i++) {
+    str +=
+      `
+       <tr>
+       <td>${person[i].name}</td>
+       <td>${person[i].nrTel}</td>
+       <td><i class="far fa-edit" onclick="edit(${i});"></i></td>
+       <td><i class="far fa-trash-alt" onclick="del(${i});"></i></td>
+      </tr>
+      `
+  }
+  document.querySelector("#addPerson tbody").innerHTML = str;
+}
+
+function edit(idx) {
+  let contact = person[idx];
+
+document.querySelector("[name='nume']").value = contact.name;
+document.querySelector("[name='nrTel']").value = contact.nrTel;
+
+index = idx;
+
+document.querySelector("#editBtn").classList.remove("hidden");
+document.querySelector("#cancelBtn").classList.remove("hidden")
+document.querySelector("#addBtn").classList.add("hidden");
+}
+
+function validate(name, phone) {
+  if (phone.length < 10) {
+    alert('Numarul este prea scurt');
+    return false;
   }
 
-  function edit(idx) {
-    let contact = person[idx];
-
-  document.querySelector("[name='nume']").value = contact.name;
-	document.querySelector("[name='nrTel']").value = contact.nrTel;
-
-  index = idx;
-
-	document.querySelector("#editBtn").classList.remove("hidden");
-  document.querySelector("#cancelBtn").classList.remove("hidden")
-	document.querySelector("#addBtn").classList.add("hidden");
+  if (name.length < 3) {
+    alert('Numele este prea scurt');
+    return false;
   }
 
-  function validate(name, phone) {
-    if (phone.length < 10) {
-      alert('Numarul este prea scurt');
-      return false;
-    }
+  return true;
+}
 
-    if (name.length < 3) {
-      alert('Numele este prea scurt');
-      return false;
-    }
-
-    return true;
-  }
-
-  function editSec() {
-    if (index === -1) {
-        return;
-    }
-
-    let name = document.querySelector("[name='nume']").value;
-    let phone = document.querySelector("[name='nrTel']").value;
-
-    if (!validate(name, phone)) {
+function editSec() {
+  if (index === -1) {
       return;
-    }
-
-    person[index].name = name;
-    person[index].nrTel = phone;
-
-    draw();
-
-	cancel();
   }
 
-  function cancel(){
-    index = -1;
+  let name = document.querySelector("[name='nume']").value;
+  let phone = document.querySelector("[name='nrTel']").value;
 
-    document.querySelector("#editBtn").classList.add("hidden");
-    document.querySelector("#addBtn").classList.remove("hidden");
-    
-    document.querySelector("form").reset();
+  if (!validate(name, phone)) {
+    return;
+  }
+
+  person[index].name = name;
+  person[index].nrTel = phone;
+
+  draw();
+
+cancel();
+}
+
+function cancel(){
+  index = -1;
+
+  document.querySelector("#editBtn").classList.add("hidden");
+  document.querySelector("#addBtn").classList.remove("hidden");
+  
+  document.querySelector("form").reset();
 
 }
 
-  function del(idx) {
-      if(confirm(`Esti sigur ca vrei sa stergi contactul ${person[idx].name}?`))
-    person.splice(idx,1);
-    draw();
+function del(idx) {
+    if(confirm(`Esti sigur ca vrei sa stergi contactul ${person[idx].name}?`))
+  person.splice(idx,1);
+  draw();
+}
+
+function adauga() {
+  let name = document.querySelector("[name='nume']").value;
+  let nrTel = document.querySelector("[name='nrTel']").value;
+
+  if (!validate(name, nrTel)) {
+    return;
   }
 
-  function adauga() {
-    let name = document.querySelector("[name='nume']").value;
-    let nrTel = document.querySelector("[name='nrTel']").value;
-  
-    if (!validate(name, nrTel)) {
-      return;
-    }
+  person.push({
+    "name": name,
+    "nrTel": nrTel
+  });
 
-    person.push({
-      "name": name,
-      "nrTel": nrTel
-    });
+  draw();
+  document.querySelector("form").reset();
+}
 
-    draw();
-    document.querySelector("form").reset();
+document.querySelector("[name='nrTel']").addEventListener('keyPress', function (e) {
+  console.info(e, e.keyCode);
+  if (e.keyCode === 13) {
+    adauga();
   }
-
- 
+});
